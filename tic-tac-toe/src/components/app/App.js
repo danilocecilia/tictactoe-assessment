ce400929dd46cee9abd5894397e83e91316ccd74
unknown <danilo.cecilia@gmail.com>
2019-02-06 23:00:27 -0800

    Added buttons that will handle some state changes, also added some styles and updated the readme file

--- tic-tac-toe/src/components/app/App.js
+++ tic-tac-toe/src/components/app/App.js
@@ -7,8 +7,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1 className="center">Tic Tac Toe</h1>
-        <h2 className="center">Single Player vs Computer</h2>
+        <h1 className="center-text">Tic Tac Toe</h1>
+        <h2 className="center-text">Single Player vs Computer</h2>
+        <div className="align-center">
+          <button className="back">Undo</button>
+          <button className="site">Restart Game</button>
+          <button className="button">Redo</button>
+        </div>
         <GameBoard />
       </div>
     );
