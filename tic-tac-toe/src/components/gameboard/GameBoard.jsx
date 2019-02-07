48845eb6e89cbe266f2054abbf4053e7e8b7ce91
unknown <danilo.cecilia@gmail.com>
2019-02-07 01:32:57 -0800

    Added some logic for creating the boxes dinamically and also generating the board using a of 9 positions. Also handling the restartgame, clearing the text for all the boxes.

--- tic-tac-toe/src/components/gameboard/GameBoard.jsx
+++ tic-tac-toe/src/components/gameboard/GameBoard.jsx
@@ -3,10 +3,41 @@ import "./GameBoard.css";
 import Box from "./box/Box";
 
 export default class GameBoard extends Component {
+  state = {
+    restart: false
+  };
+
+  handleRestartGame = () => {
+    return this.setState({ restart: true });
+  };
+
+  renderBoxes = () => {
+    // Building an array of nine possible positions
+    const array = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+    return array.map((value, index) => {
+      return (
+        <Box
+          key={index}
+          id={value}
+          restartGame={this.state.restart}
+          text={"O"}
+        />
+      );
+    });
+  };
+
   render() {
     return (
-      <div className="game-board">
-        <Box />
+      <div>
+        <div className="align-center">
+          <button className="back">Undo</button>
+          <button className="site" onClick={() => this.handleRestartGame()}>
+            Restart Game
+          </button>
+          <button className="button">Redo</button>
+        </div>
+        <div className="game-board">{this.renderBoxes()}</div>
       </div>
     );
   }
--- tic-tac-toe/src/components/gameboard/box/Box.jsx
+++ tic-tac-toe/src/components/gameboard/box/Box.jsx
@@ -2,19 +2,20 @@ import React, { Component } from "react";
 import "./Box.css";
 
 export default class Box extends Component {
+  state = {
+    text: ""
+  };
+
+  handleClick = () => {
+    this.setState({ text: this.props.text });
+  };
+
   render() {
+    const { restartGame } = this.props;
     return (
-      <>
-        <div className="box" />
-        <div className="box" />
-        <div className="box" />
-        <div className="box" />
-        <div className="box" />
-        <div className="box" />
-        <div className="box" />
-        <div className="box" />
-        <div className="box" />
-      </>
+      <div onClick={e => this.handleClick()} className="box">
+        {restartGame ? "" : this.state.text}
+      </div>
     );
   }
 }
