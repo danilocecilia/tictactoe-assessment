d6f2a04e176e80e033e40ba7696ce03fc0d62791
unknown <danilo.cecilia@gmail.com>
2019-02-07 01:31:10 -0800

    Moved buttons to GameBoard components, so this way is easy to manage the states and helps separate concerns.

--- tic-tac-toe/src/components/app/App.js
+++ tic-tac-toe/src/components/app/App.js
@@ -9,11 +9,6 @@ class App extends Component {
       <div>
         <h1 className="center-text">Tic Tac Toe</h1>
         <h2 className="center-text">Single Player vs Computer</h2>
-        <div className="align-center">
-          <button className="back">Undo</button>
-          <button className="site">Restart Game</button>
-          <button className="button">Redo</button>
-        </div>
         <GameBoard />
       </div>
     );
