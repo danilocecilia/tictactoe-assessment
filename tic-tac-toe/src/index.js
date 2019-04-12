1e6532634c13bccdd2677a450f46b619d5811e0e
Danilo <danilo@Danilos-MacBook-Pro.local>
2019-04-12 13:34:09 -0700

    test commit

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 
-import "./index.css";
 import App from "./components/app/App";
 import boardGameApp from "./reducers";
 
